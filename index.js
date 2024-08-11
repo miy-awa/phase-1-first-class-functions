625fcbafb1916c303eb9bdb8bc6de94cf946e7b7
miyawa <raymiyawa6@gmail.com>
2024-08-11 16:19:49 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+/*exerciseRoutine(function () {
+    console.log("Stretch! Work that core!");
+});*/
+
+/*function morningRoutine(exercise) {
+    let breakfast;
+
+    if (exercise === liftWeights) {
+        breakfast = "protein bar";
+    } else if ( exercise === swimFortyLaps) {
+        breakfast = "kale smoothie";
+    } else {
+        breakfast = "granola";
+    }
+
+    exerciseRoutine(exercise);
+
+    return function () {
+        console.log(`Nom nom nom, this ${breakfast} is delicious!`);
+    };
+}
+
+const afterExercise = morningRoutine(liftWeights);
+afterExercise;*/
